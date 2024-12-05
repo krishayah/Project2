97eccb323dcb729c1e710db482223ec9180b8eac
krishayah <krishagurung26@gmail.com>
2024-12-04 21:03:16 -0600

    added add_task function

---
+++ Logic.py
@@ -0,0 +1,27 @@
+from PyQt6.QtCore import Qt
+
+class APPLogic:
+    def __init__(self):
+        #Structure: {'id': int, 'name': str, 'category': str, 'priority': str, 'completed': bool}
+        self.tasks = []
+        self.task_id_counter = 0
+
+    def add_task(self, task_input):
+
+        try:
+            name, category, priority = map(str.strip, task_input.split(","))
+            self.task_id_counter += 1
+            task = {
+                "id": self.task_id_counter,
+                "name": name,
+                "category": category,
+                "priority": priority,
+                "completed": False,
+            }
+            self.tasks.append(task)
+            return "Task added successfully!"
+        except ValueError:
+            return "Error: Task input should be in the format 'name, category, priority'."
+
+
+
